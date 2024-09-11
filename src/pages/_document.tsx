718eaa66ec4959cad15f7027947cbc15f02feaf3
Abdelmuttalib <hagar9886@gmail.com>
2024-09-12 04:04:09 +0800

    - seo2

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,6 @@ export default function Document() {
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="theme-color" content="#ffffff" />
 
         {/* SEO Meta Tags */}
         <meta
