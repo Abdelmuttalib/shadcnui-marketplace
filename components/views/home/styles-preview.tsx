cd0ee75f083ab8c21d304e2cec0ad7c3ddf4425c
Abdelmuttalib <hagar9886@gmail.com>
2025-06-13 23:11:58 +0800

    - remove link from style preview card in landing page

--- components/views/home/styles-preview.tsx
+++ components/views/home/styles-preview.tsx
@@ -13,9 +13,9 @@ export function StylesPreview() {
             key={style.name}
             title={style.title}
             style={style.name}
-            link={{
-              href: `/styles/${style.name}`,
-            }}
+            // link={{
+            //   href: `/styles/${style.name}`,
+            // }}
           />
         ))}
     </>
